41320c4ad4c2f094a74e5c676000d2817ae87440
toneyrek <ralekdwhiz@gmail.com>
2022-12-08 05:55:50 -0800

    size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,6 +5,10 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	printf(*Size of a char: %lu byte(s)\n", sizeof(char));					
+	printf(*Size of an int: %lu byte(s)\n", sizeof(int));
+	printf(*Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf(*Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf(*Size of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
 }
