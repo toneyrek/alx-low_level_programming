bd08eb84af868e6cc05f25c24b9c9b6eb20bab43
toneyrek <ralekdwhiz@gmail.com>
2022-12-14 04:41:58 -0800

    task 1

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if intefer is positive or negative
+ * Return: zero
+ */
+
+int main(void)
+{
+int i;
+
+i= 0;
+positive or negative(i);
+
+return (0);
+}
