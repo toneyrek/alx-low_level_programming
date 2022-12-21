8b0fbe214b15e28e0cb2e4713eb6d04b3b263fe7
toneyrek <ralekdwhiz@gmail.com>
2022-12-21 13:36:28 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,8 +10,8 @@ char *_strcat(char *dest, char *src)
 {
 int a = -1, i;
 
-for (i = 0; dest[i] != '\0')
-i++;
+for (i = 0; dest[i] != '\0';)
+	i++;
 
 do {
 	a++;
@@ -19,6 +19,7 @@ do {
 	i++;
 
 } while (src[a] != '\0');
+
 return (dest);
 
 }
