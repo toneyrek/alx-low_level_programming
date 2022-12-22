9dd5e8c2a1bb5e216607852208a13a7fa510cfa0
toneyrek <ralekdwhiz@gmail.com>
2022-12-21 22:31:05 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,43 +1,26 @@
 #include "main.h"
-
 /**
- * rot13 - encodes a string using rot13
- * @str: the string targeted
- * Return: returns the encoded string
+ * *rot13 - function that encodes string using rot13
+ * @s: string
+ * Return: encoded string
  */
-
-char *rot13(char *str)
+char *rot13(char *s)
 {
-	int index1, index2;
+	int x, y;
 
-	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F'
-		'G', 'H', 'I', 'J', 'K', 'L',
-		'M', 'N', 'O', 'P', 'Q', 'R',
-		'S', 'T', 'U', 'V', 'W', 'X',
-		'Y', 'Z', 'a', 'b', 'c', 'd',
-		'e', 'f', 'g', 'h', 'i', 'j',
-		'k', 'l', 'm', 'n', 'o', 'p',
-		'q', 'r', 's', 't', 'u', 'v',
-		'w', 'x', 'y', 'z'};
-	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-		'T', 'U', 'V', 'W', 'X', 'Y',
-		'Z', 'A', 'B', 'C', 'D', 'E',
-		'F', 'G', 'H', 'I', 'J', 'K',
-		'L', 'M', 'n', 'o', 'p', 'q',
-		'r', 's', 't', 'u', 'v', 'w',
-		'x', 'y', 'z', 'a', 'b', 'c',
-		'd', 'e', 'f', 'g', 'h', 'i',
-		'j', 'k', 'l', 'm'};
-	while (str[++index++])
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (x = 0; s[x] != '\0'; x++)
 	{
-		for (index2 = 0; index2 < 52; index2++)
+		for (y = 0; input[y] != '\0'; y++)
 		{
-			if (str[index] == alphabet[index2])
+			if (s[x] == input[y])
 			{
-				str[index] = rot13key[index2];
+				s[x] = output[y];
 				break;
 			}
 		}
 	}
-	return (str);
+	return (s);
 }
