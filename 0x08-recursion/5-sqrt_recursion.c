16e5603fe93d36be55714d88e6c1685a7bbaa36a
toneyrek <ralekdwhiz@gmail.com>
2023-01-04 03:11:30 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,8 +1,5 @@
 #include "main.h"
 
-int find_sqrt(int num, int root);
-int _sqrt_recursion(int n);
-
 /**
  * find_sqrt - Finds the natural square root of an inputted number
  * @num: The number to find the square root of
@@ -15,4 +12,5 @@ int find_sqrt(int num, int root)
 {
 	if ((root * root) == num)
 		return (root);
+}
 
