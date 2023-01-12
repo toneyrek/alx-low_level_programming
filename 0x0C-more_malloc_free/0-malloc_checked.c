7a128bd8ca39a2d3b5c376a51e247c76e0909b52
toneyrek <ralekdwhiz@gmail.com>
2023-01-12 15:25:49 -0800

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * malloc_checked - array for prints a string
+ * @b: number of meory
+ * Return: void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+void *pointer = malloc(b);
+{
+void *p;
+
+p = malloc(b);
+if (p == NULL)
+{
+exit(98);
+}
+
+return (p);
+}
+
